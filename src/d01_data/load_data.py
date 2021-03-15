4543e498f6270e10d20e39adea1b73195e2397cf
charles-42 <charles.beniac@gmail.com>
2021-03-15 21:49:53 +0100

    exemple data_cleaning

--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -1,23 +1,14 @@
 
 import pandas as pd
-import sqlalchemy
-import mysql.connector
-
-from conf.conf import mysql_pseudo, mysql_mdp
-
+from src.d00_utils.mysql_utils import mysql_connect, save_to_mysql
 
 # After dowloading my csv and xlsx files, I read them with pandas
-df = pd.read_excel(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx", skiprows=3)
-df2 = pd.read_csv(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/DataAnalyst.csv")
+df1 = pd.read_excel(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/2020_Data_Professional_Salary_Survey_Responses.xlsx", skiprows=3)
+dfk = pd.read_csv(r"/home/apprenant/PycharmProjects/American_dream/Data/01_raw/DataAnalyst.csv")
 
-# I create a connexion with mysql server thanks to mysql connector
-mysql_username = mysql_pseudo
-mysql_password = mysql_mdp
-database_name = 'american_dream_db'
-database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@localhost/{2}'.
-                                               format(mysql_username, mysql_password, database_name), pool_recycle=1, pool_timeout=57600).connect()
+#Create connection with mysqm
+connect = mysql_connect()
 
-# I  transpose the table in mysql
-df.to_sql(con=database_connection, name='survey_1', if_exists='replace')
-df2.to_sql(con=database_connection, name='survey_2', if_exists='replace')
-database_connection.close()
+# Save the table in mysql database
+save_to_mysql(db_connect=connect,df_to_save=df1,df_name='survey_1')
+save_to_mysql(db_connect=connect,df_to_save=dfk,df_name='survey_k')
--- src/d02_intermediate/cleaning.py
+++ src/d02_intermediate/cleaning.py
@@ -1,5 +1,1 @@
-print(3)
-
-from src.d01_data.load_data import plus_cinq
-
-print(plus_cinq(3))
+# here I can write
